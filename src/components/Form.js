e8d815313ed09f96060f8df1e0dd92beb9d2b74c
faizaniq <github email address>
2019-04-10 11:04:34 -0400

    done

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,29 @@
 import React from "react";
 
 class Form extends React.Component {
+
+  state = {
+    value: ''
+  }
+
+  // handleChange = (e) => {
+  // }
+  //
+  // handleSubmit = (e) => {
+  //   event.preventDefault()
+  // }
+
+
   render() {
-    return <h1>{/*create form*/}</h1>;
+    return (
+      <h1>{/*create form*/}</h1>
+      // <form>
+      //   <input type="text" value={this.state.value} onChange={this.handleChange} name="title" placeholder="title" />
+      //   <input type="text" value={this.state.value} onChange={this.handleChange} name="author" placeholder="author" />
+      //   <input type="text" value={this.state.value} onChange={this.handleChange} name="img" placeholder="img" />
+      //   <button>Submit<button/>
+      // </form>
+    )
   }
 }
 
