0247844da1c5da5f119db8bd8bd64132072b5930
github username <github email address>
2019-04-10 14:11:52 -0400

    sorry for late git add :P

--- src/components/Form.js
+++ src/components/Form.js
@@ -16,12 +16,17 @@ class Form extends React.Component {
   onSubmitHandler = (e) => {
     e.preventDefault();
     this.props.onSubmitHandler(this.state)
+    this.setState({
+      title: '',
+      author: '',
+      img:''
+    })
   }
 
   render() {
     return (
       <form onSubmit={this.onSubmitHandler}>
-        <input type="text" name="title" value={this.state.bookTitle} placeholder="Book Title" onChange={this.onChangeHandler}/>
+        <input type="text" name="title" value={this.state.title} placeholder="Book Title" onChange={this.onChangeHandler}/>
         <input type="text" name="author" value={this.state.author} placeholder="Author" onChange={this.onChangeHandler}/>
         <input type="text" name="img" value={this.state.img} placeholder="Book Image" onChange={this.onChangeHandler}/>
         <button>Submit</button>
